4985052518b06d29d80a6cf997a706eab8c24861
Summer <939648675@qq.com>
2024-02-18 02:50:02 +0800

    初次提交
    Signed-off-by: Summer <939648675@qq.com>

--- spring-blog/src/main/java/com/hncpu/blog/BlogApplication.java
+++ spring-blog/src/main/java/com/hncpu/blog/BlogApplication.java
@@ -1,5 +1,4 @@
 package com.hncpu.blog;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
